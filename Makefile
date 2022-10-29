c3569323b87581dbfc3d54bf816309b02c9b2b4a
Romanhessec <romangabimarian@gmail.com>
2022-10-29 11:53:47 +0300

    first commit

---
+++ Makefile
@@ -0,0 +1,4 @@
+build:
+	gcc tema1.c -o tema1 -lpthread -Wall -Werror 
+clean:
+	rm tema1
---
+++ tema1.c
@@ -0,0 +1,13 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void get_args(int argc, char **argv)
+{
+	
+}
+
+int main(int argc, char* argv[]){
+    get_args(argc, argv);
+}
